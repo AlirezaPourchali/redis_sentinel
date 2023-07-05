4bcd6a475b52bd9cb94372bde5e9d13bfcb61215
alireza <alireza>
2023-07-05 14:28:17 +0330

    redis master can be accessed from sentinels

--- redis_get.py
+++ redis_get.py
@@ -1,7 +1,14 @@
 from redis.sentinel import Sentinel
-
+import redis 
 sentinel_instance = Sentinel([('localhost' , 1001),('localhost' , 1002),('localhost' , 1003)])
 
-current_master = sentinel_instance.master_for('master-name' , socket_timeout=0.1)
+host , port = sentinel_instance.discover_master('mymaster')
 
-print(current_master)
+print(host , port)
+
+server = redis.Redis(host , port)
+
+server.set("ali" , "asghar")
+
+ali = server.get("ali")
+print(ali.decode())
